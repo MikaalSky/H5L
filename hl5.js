6f553179f9422bc87fb44052a6e227e769878295
MikaalSky <lokio27productions@gmail.com>
2016-04-13 09:35:19 -0500

    Added h5l-wrapper and fixed names

--- hl5.js
+++ hl5.js
@@ -8,10 +8,11 @@ var hl5 = function(wrapperid){
 	this.instance = HL5.instanceCount;
 	HL5.instanceCount++;
 	this.main = document.getElementById("myHL5");
+	this.main.className+=" h5l-wrapper"
 	var editorContainer = document.createElement("div");
 	editorContainer.id = "hl5-editor-" + this.instance;
 	editorContainer.innerHTML = "dd"
-	editorContainer.className += "hl5-editor"
+	editorContainer.className += " h5l-editor"
 	this.main.appendChild(editorContainer);
 	this.editor = ace.edit("hl5-editor-" + this.instance);
 	this.editor.getSession().setMode("ace/mode/javascript");
